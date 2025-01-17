93a7804053ca111ab11bc97ed6f4d02d31379e57
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-01-17 17:55:37 +0100

    Create fitnessapp
    Create fitnessapp following a youtube video made by JavaScript Mastery channel

---
+++ fitnessapp/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Logo from '../assets/images/Logo-1.png'
+
+const Footer = () => {
+  return (
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='4px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' wudtg='200px' height='40px' />
+        <Typography variant='h5' pb='40px' mt='40px'>
+          Made following a <a href='https://www.youtube.com/watch?v=KBpoBc98BwM' target='_blank' rel='noopener noreferrer' style={{color: '#000977', textDecoration: 'none'}}>JavaScript Mastery youtube video</a> 
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Footer
---
+++ fitnessapp/src/components/SimilarExercises.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import HorizontalScrollbar from './HorizontalScrollbar'
+import Loader from './Loader'
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ mt: { lg: '100px', xs: '0' }}}>
+      <Typography variant='h3' mt={2} mb={5}>
+        Exercises that target the same muscle group
+      </Typography>
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100%',
+          p: '20px'
+        }}
+      >
+        {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
+      </Box>
+      <Typography variant='h3' mt={2} mb={5}>
+        Exercises that use the same equipment
+      </Typography>
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100%',
+          p: '20px'
+        }}
+      >
+        {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
+      </Box>
+    </Box>
+  )
+}
+
+export default SimilarExercises
