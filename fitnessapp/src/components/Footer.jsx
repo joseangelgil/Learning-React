a8a02d0b62abf971406f2ef16f7d9211cd4efe9a
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-01-21 11:32:21 +0100

    Create mouse-follower project

--- fitnessapp/src/components/Footer.jsx
+++ fitnessapp/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <Box mt='80px' bgcolor='#fff3f4'>
       <Stack gap='4px' alignItems='center' px='40px' pt='24px'>
-        <img src={Logo} alt='logo' wudtg='200px' height='40px' />
+        <img src={Logo} alt='logo' width='200px' height='40px' />
         <Typography variant='h5' pb='40px' mt='40px'>
           Made following a <a href='https://www.youtube.com/watch?v=KBpoBc98BwM' target='_blank' rel='noopener noreferrer' style={{color: '#000977', textDecoration: 'none'}}>JavaScript Mastery youtube video</a> 
         </Typography>
--- tradingking/src/components/StockList.jsx
+++ tradingking/src/components/StockList.jsx
@@ -43,7 +43,7 @@ export const StockList = () => {
           setStock(data)
         }
       } catch (err) {
-
+          console.log(err)
       }
     }
     fetchData()
